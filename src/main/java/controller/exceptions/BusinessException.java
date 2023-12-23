280130027d0bd836e305b433e11fa47b9ae66d95
Wanielton F. Silva <wanieltonferreira@gmail.com>
2023-12-23 02:31:18 -0300

    .

--- src/main/java/controller/exceptions/BusinessException.java
+++
@@ -1,9 +0,0 @@
-package controller.exceptions;
-
-public class BusinessException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public BusinessException(String msg) {
-		super(msg);
-	}
-}
